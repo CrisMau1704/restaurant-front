505288466aee256ab98fe31660ba470de3c0dfc7
CrisMau1704 <cristian76724599@gmail.com>
2025-07-28 22:50:38 -0400

    SUBI2

--- src/services/Api.services.js
+++ src/services/Api.services.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-console.log('VITE_API_URL desde env:', import.meta.env.VITE_API_URL);
+console.log('Base URL usada para API:', urlBaseMeta);
+
 const urlBaseMeta = import.meta.env.VITE_API_URL + "/api";// Para verificar la URL en consola
 
 
